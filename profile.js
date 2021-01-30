3c2a41f08d0fd69a8cfa28f0b0d93df9c6f59fc2
Aaron <aaron.a.aguinaldo@gmail.com>
2021-01-30 13:38:07 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openCity(cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -8,13 +8,14 @@ function openCity(evt, cityName) {
     tabcontent[i].style.display = "none";
   }
 
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  }
+  
+/*
   // Get all elements with class="tablinks" and remove the class "w3-white"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" w3-white", "");
   }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " w3-white;
-  }
+*/
